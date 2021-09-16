a1f14c25f71437e8dad6d3d5a278b78b9d49c844
psychoactiv <pritam54majumdar@gmail.com>
2021-09-16 01:24:30 -0700

    Feat
    added several features

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 var response = document.querySelector(".btn-1");
 var textspace = document.querySelector("#area");
 var welcome = document.querySelector(".box");
+var img = document.querySelector(".img-btn");
 
 var userClicked = 1;
 
-response.addEventListener("click", function clickEventHandler() {
-  welcome.innerHTML = textspace.value;
-});
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+var tap = 1;
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with server! try again after some time");
+}
+
+function clickEventHandler() {
+  var inputText = textspace.value;
+
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      welcome.innerText = translatedText;
+    })
+    .catch(errorHandler);
+  if (tap % 2 == 0) {
+    img.src = "img/minion_2.jpg";
+  } else {
+    img.src = "img/458566.jpg";
+  }
+  tap++;
+}
+
+response.addEventListener("click", clickEventHandler);
